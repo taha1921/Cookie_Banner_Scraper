a4e1c0b9f3fb19c597a25ee2fbdc6efd00449f60
taha1921 <taha.red70@gmail.com>
2025-09-01 00:36:45 +0200

    Updated for union logic for reject button selectors + functionality to click on preferences button

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from playwright.async_api import async_playwright
 import asyncio
-from sklearn.model_selection import train_test_split
 import json
 import logging
+from datetime import datetime
+import re
 
 # data = pd.read_excel('Thesis_Data_2.xlsx', sheet_name='Data')
 # explicit_domains = data[data['Implicit vs Explicit'] == 'Explicit']
@@ -20,40 +21,47 @@ import logging
 """
 Takes as input the button elements inside a banner from Playwright and assesses whether there is a reject button on the first layer of the banner.
 """
-async def check_for_reject_button(banner, selector):
-
-    # attribute = selector["attribute"]
+async def check_for_reject_button(banner, selector, css_selectors, playwright_selectors):
+    # First we check if the specific selector for this selector can be found in the banner
     reject_button = selector['reject_button']
+    # count_main = await banner.locator(reject_button).count()
+    # count_alt = await banner.locator(','.join(reject_button_values)).count()
 
     if(await banner.locator(reject_button).count() > 0):
-        logging.info(f"Found reject button with selector: {reject_button}")
+        logging.info(f"Found reject button with the designated selector: {reject_button}")
+        return 1
+    elif(await banner.locator(','.join(css_selectors)).count() > 0):
+        logging.info(f"Found reject button with an alternative selector from the the list")
         return 1
     else:
-        logging.debug(f"No reject button found with selector: {reject_button}")
-        return 0
-
-    btn_count = await buttons.count()
-    # print(btn_count)
-    for i in range(btn_count):
-        btn = buttons.nth(i)
-        btn_id = await btn.get_attribute(attribute)
-        # print(btn_id)
-        if(btn_id == reject_button):
-            return 1
-        # else:
-        #     text_content = await btn.text_content()
-        #     text = text_content.strip()
-        #     if "reject" in text.lower():
-        #         return 1
-
-    return 0
+        if('preferences_button' in selector):
+            try:
+                await banner.locator(selector['preferences_button']).first.click()
+                logging.info(f"Clicked preferences button with selector: {selector['preferences_button']}")
+                try:
+                    await banner.locator(reject_button).wait_for(state='attached', timeout=10000)
+                    logging.info(f"Found reject button with the designated selector on layer 2: {reject_button}")
+                    return 2
+                except:
+                    try:
+                        await banner.locator(','.join(css_selectors)).wait_for(state='attached', timeout=10000)
+                        logging.info(f"Found reject button with an alternative selector from the the list on layer 2")
+                        return 2
+                    except:
+                        logging.debug(f"No reject button found with selector: {reject_button}")
+                        return 0
+            except Exception as e:
+                logging.error(f"Could not find preferences button or reject button: {e}")
+                return 0
+        else:
+            logging.debug(f"No reject button found with selector: {reject_button}")
+            return 0
 
 """
 Takes an individual selector and searches the page for it. If the selector is found then we return the locator object 
 and the selector.
 """
 async def locate_cookie_banner(page, selector):
-
     try:
         banner = page.locator(selector)
         await banner.wait_for(state='attached', timeout=10000)
@@ -61,7 +69,6 @@ async def locate_cookie_banner(page, selector):
             logging.info(f"Found banner with selector: {selector}")
             return banner, selector
     except Exception as e:
-        # logging.error(f"Error or Possible timeout when searching for banner: {e}")
         return None
     
 """
@@ -69,9 +76,7 @@ Takes a list of selectors and launches parallel tasks to search for each selecto
 return a result will be the one that we use. If no selector is found then we return None. 
 """
 async def check_selectors(page, selectors):
-
     tasks = [asyncio.create_task(locate_cookie_banner(page, sel)) for sel in selectors.keys()]
-    # done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
     try:
         for task in asyncio.as_completed(tasks):
             result = await task
@@ -87,15 +92,22 @@ async def check_selectors(page, selectors):
 """
 Runs our simulator which takes a list of domains and a list of CMP selectors to try and find the cookie banner on the page.
 """
-async def simulator(domain_list, selectors):
+async def simulator(domain_list, selectors, css_selectors, playwright_selectors):
     reject_all_presence = []
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=False)
-
+        
         for sample_domain in domain_list:
             page = await browser.new_page()
-            await page.goto(sample_domain)
-            logging.info(f"Visiting domain: {sample_domain}")
+            try:
+                await page.goto(sample_domain)
+                logging.info(f"Visiting domain: {sample_domain}")
+            except:
+                logging.error(f"Could not load domain: {sample_domain}")
+                await page.close()
+                reject_all_presence.append(-2)
+                continue
+
             await page.mouse.move(100, 200)
             await page.mouse.wheel(0, 300)
 
@@ -105,12 +117,11 @@ async def simulator(domain_list, selectors):
                 if result:
                     banner, selector = result
                     selector_properties = selectors[selector]
-                    # buttons = banner.locator(selector_properties['element'])
-                    # print(buttons)
-                    # print(banner)
-                    # buttons = banner.get_by_role('button')
-                    res = await check_for_reject_button(banner, selector_properties)
-                    # print(f"Reject all button presence for domain {sample_domain}: {res}")
+                    res = await check_for_reject_button(banner, selector_properties, css_selectors, playwright_selectors)
+                    if(res == 0):
+                        await page.screenshot(path=f'../output/screenshots/{sample_domain.replace("https://", "").replace("http://", "").replace(".", "_")}_no_reject.png')
+                    elif(res == -2):
+                        await page.screenshot(path=f'../output/screenshots/{sample_domain.replace("https://", "").replace("http://", "").replace(".", "_")}.png')
                     reject_all_presence.append(res)
 
                 else:
@@ -128,38 +139,48 @@ async def simulator(domain_list, selectors):
 
 
 async def process_domains():
-
+    # Start Logging
     logging.basicConfig(
-    filename='../output/logs/train_run.log',      
+    filename = f"../output/logs/val_run_{datetime.now():%Y-%m-%d_%H-%M-%S}.log",
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s'
     )
 
+    # Load Domains
     train = pd.read_excel('../input_files/train.xlsx')
-    domains_of_interest = train[(train['Reject All Option'] == True) & (train['Number of Interactions to Reject'] == 1)]
-    domain_list = domains_of_interest['Domain'].tolist()
+    domain_list = ['https://sinsay.com', 'https://247sports.com', 'https://zalando.de', 'https://bibleserver.com']
+    # domain_list = train['Domain'].tolist()[0:10]
     domain_list = ['https://'+domain if "https://" not in domain else domain for domain in domain_list ]
     logging.info(f"Number of domains to process: {len(domain_list)}")
     # print(len(domain_list))
 
+    # Load Selectors
     with open('../SELECTORS.json', 'r') as file:
         selectors = json.load(file)
+    
+    reject_button_selectors = [selectors[key]['reject_button'] for key in selectors.keys() if 'reject_button' in selectors[key]]
+    reject_button_selectors = ",".join(reject_button_selectors).split(',')
+    reject_button_selectors = list(set(reject_button_selectors))
 
-    reject_all_presence = await simulator(domain_list, selectors)
-    total_correct = sum(1 for res in reject_all_presence if res == 1)
-    total_incorrect = sum(1 for res in reject_all_presence if res == 0)
-    total_not_found = sum(1 for res in reject_all_presence if res == -1)
+    css_selectors = []
+    playwright_selectors = []
+    
+    for selector in reject_button_selectors:
+        if(">>" in selector):
+            playwright_selectors.append(selector)
+        else:
+            css_selectors.append(selector)
+    
+    print(playwright_selectors)
+
+    reject_all_presence = await simulator(domain_list, selectors, css_selectors, playwright_selectors)
+    total_found = sum(1 for res in reject_all_presence if res in [1,2])
+    total_not_found = sum(1 for res in reject_all_presence if res == 0)
+    total_no_banner = sum(1 for res in reject_all_presence if res == -1)
     total_error = sum(1 for res in reject_all_presence if res == -2)
-    logging.info(f"Total Correct Results: {total_correct}")
-    logging.info(f"Total Incorrect Results: {total_incorrect}")
-    logging.info(f"Total Not Found Results: {total_not_found}")
+    logging.info(f"Total Correct Results: {total_found}")
+    logging.info(f"Total Incorrect Results: {total_not_found}")
+    logging.info(f"Total Not Found Results: {total_no_banner}")
     logging.info(f"Total Error Results: {total_error}")
-    # print(reject_all_presence)
-    # domains_of_interest['Automated_Reject_All'] = reject_all_presence
-    # accuracy = len(domains_of_interest[((domains_of_interest['Automated_Reject_All'] == 1) & (domains_of_interest['Reject All Option'] == True) & (domains_of_interest['Number of Interactions to Reject'] == 1)) | 
-    #             ((domains_of_interest['Automated_Reject_All'] == 0) & (domains_of_interest['Reject All Option'] == False))])
-    # print(f"Total Correct Results: {accuracy}")
-
-    # domains_of_interest.to_csv('domains_reject_all.csv', index=False)
-
+    
 asyncio.run(process_domains())
