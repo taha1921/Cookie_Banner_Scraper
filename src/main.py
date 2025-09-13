4cd4211ad4b6585c1d20b4b09f5297e6e8a664b0
taha1921 <taha.red70@gmail.com>
2025-09-13 12:19:14 +0200

    .

--- src/main.py
+++ src/main.py
@@ -229,7 +229,3 @@ async def process_domains(domain_list, progress_callback=None, aggressive_mode=F
     os.makedirs(f"../output/results", exist_ok=True)
     results_df.to_csv(f"../output/results/run_{datetime.now():%Y-%m-%d_%H-%M-%S}.csv", index=False)
 
-    # for testing
-    # val_df = pd.read_excel("../input_files/test.xlsx")
-    # merged_df = val_df.merge(results_df, on="Domain", how="left")
-    # merged_df.to_csv(f"../output/results/test_{datetime.now():%Y-%m-%d_%H-%M-%S}.csv", index=False)
